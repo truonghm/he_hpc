7157ebf110a54e4fbff29655e41be2818a9ff20d
Truong Hoang <truong.hoang@cel-consulting.com>
2023-11-12 16:57:52 +0700

    add whe

--- src/he_kernels.py
+++ src/he_kernels.py
@@ -33,7 +33,6 @@ def compute_hist(src, hist):
 @cuda.jit
 def compute_cdf(hist, cdf, hist_sum):
     shist = cuda.shared.array(256, dtype=np.uint32)
-    scdf = cuda.shared.array(256, dtype=np.float32)
 
     tx = cuda.threadIdx.x
     idx = cuda.blockIdx.x * cuda.blockDim.x + tx
@@ -44,16 +43,25 @@ def compute_cdf(hist, cdf, hist_sum):
         shist[tx] = 0
     cuda.syncthreads()
 
-    if tx == 0:
-        scdf[tx] = shist[tx]
-    for i in range(1, 256):
+    stride = 1
+    while stride < 256:
+        index = (tx + 1) * stride * 2 - 1
+        if index < 256:
+            shist[index] += shist[index - stride]
+        stride *= 2
         cuda.syncthreads()
-        if tx >= i:
-            scdf[tx] = scdf[tx] + shist[tx - i]
-    cuda.syncthreads()
+
+    stride = 128
+    while stride > 0:
+        index = (tx + 1) * stride * 2 - 1
+        if index + stride < 256:
+            shist[index + stride] += shist[index]
+        stride //= 2
+        cuda.syncthreads()
 
     if idx < hist.shape[0]:
-        cdf[idx] = min(255, (round((scdf[tx] / hist_sum) * 255)))
+        cdf[idx] = min(255, (round((shist[tx] / hist_sum) * 255)))
+
 
 
 @cuda.jit
@@ -89,3 +97,31 @@ def compute_adjusted_hist(src, hist, lambda_val):
         weight_uniform = lambda_val / (1 + lambda_val)
         adjusted_hist_val = weight_original * original_hist_val + weight_uniform * uniform_hist_val
         cuda.atomic.add(hist, tx, int(adjusted_hist_val * (src.shape[0] * src.shape[1])))
+
+@cuda.jit
+def compute_weighted_hist(src, hist, W, lambda_, u):
+    local_hist = cuda.shared.array(256, dtype=np.float32)
+
+    x, y = cuda.grid(2)
+    tx = cuda.threadIdx.x
+    ty = cuda.threadIdx.y
+    bx = cuda.blockIdx.x
+    by = cuda.blockIdx.y
+    bw = cuda.blockDim.x
+    bh = cuda.blockDim.y
+    block_x = bx * bw + tx
+    block_y = by * bh + ty
+
+    if tx < 256:
+        local_hist[tx] = 0
+    cuda.syncthreads()
+
+    if x < src.shape[0] and y < src.shape[1]:
+        pixel_value = src[x, y, 2]
+        weight = W[block_x * block_y]
+        cuda.atomic.add(local_hist, pixel_value, weight)
+    cuda.syncthreads()
+
+    if tx < 256:
+        weighted_hist_value = (1 - lambda_) * local_hist[tx] + lambda_ * u[tx]
+        cuda.atomic.add(hist, tx, weighted_hist_value)
--- whe.py
+++ whe.py
@@ -27,6 +27,18 @@ if __name__ == "__main__":
 
     h, w, c = image.shape
 
+    # W = np.random.rand(256) 
+    W = np.ones(256, dtype=np.float32)
+
+    for i in range(128):
+        W[i] = 2
+    for i in range(128, 256):
+        W[i] = 0.5
+
+    u = np.full(256, 1.0 / 256) 
+    lambda_ = 0.8
+
+
     image_gpu = cuda.to_device(image)
     hsv_gpu = cuda.device_array_like(image_gpu)
     he_gpu = cuda.device_array_like(image_gpu)
@@ -37,9 +49,18 @@ if __name__ == "__main__":
     grid_size = (grid_size_x, grid_size_y)
 
     rgb_to_hsv[grid_size, block_size](image_gpu, hsv_gpu)
-    hist = np.zeros((256,), dtype=np.uint32)
-    hist_gpu = cuda.to_device(hist)
-    kernels.compute_hist[grid_size, block_size](hsv_gpu, hist_gpu)
+
+
+    # hist = np.zeros((256,), dtype=np.uint32)
+    # hist_gpu = cuda.to_device(hist)
+    # kernels.compute_weighted_hist[grid_size, block_size](hsv_gpu, hist_gpu, weights_gpu)
+
+    hist_gpu = cuda.device_array(256, dtype=np.float32)
+    W_gpu = cuda.to_device(W)
+    u_gpu = cuda.to_device(u)
+    kernels.compute_weighted_hist[grid_size, block_size](hsv_gpu, hist_gpu, W_gpu, lambda_, u_gpu)
+
+
     hist = hist_gpu.copy_to_host()
     hist_sum = hist.sum()
     cdf_gpu = cuda.device_array_like(hist_gpu)
@@ -52,23 +73,7 @@ if __name__ == "__main__":
     hsv_to_rgb[grid_size, block_size](he_gpu, final_output_gpu)
     final_output = final_output_gpu.copy_to_host()
 
-    imsave(OutputPath.HE, final_output, )
+    imsave(OutputPath.WHE, final_output, )
 
     print("AME: ", ame(image, final_output))
     print("Entropy: ", entropy(final_output))
-
-    # print(image)
-    # print("-------------------")
-    # print(final_output)
-
-    
-    # hsv_output = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    # h, s, v = cv2.split(hsv_output)
-
-    # equalized_v = cv2.equalizeHist(v)
-
-    # equalized_hsv = cv2.merge([h, s, equalized_v])
-    # imsave("images/hsv_equalized_cv.png", equalized_hsv, )
-    # result_img = cv2.cvtColor(equalized_hsv, cv2.COLOR_HSV2RGB)
-    # imsave("images/hsv_equalized_cv_rgb.png", result_img, )
-
